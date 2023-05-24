bafd394e8e88e13d6d76efe41ef9f031fc68e57d
salwa-yehya <salwayehyaabuafash@gmail.com>
2023-05-24 17:54:15 +0300

    a

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import Chatbot from "./Chatbot";
 function App() {
   return (
     <>
-
-      <BrowserRouter>
+<Home />
+      {/* <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}> </Route>
           <Route path='/R3F_test' element={<Home />}> </Route>
           <Route path='/shop' element={<Shop />}> </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
 
       <Chatbot />
     </>
